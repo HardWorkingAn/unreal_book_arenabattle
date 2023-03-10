589ca4ade1e0dbfe966797bdf084c087a39b2b39
HardWorkingAn <agc780@naver.com>
2023-03-10 20:47:28 +0900

    feat : Character Model Change Select(캐릭터 모델 선택) 미완성

---
+++ Source/ArenaBattle/Public/ABCharacterSelectWidget.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ABCharacterSelectWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API UABCharacterSelectWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
